07bdfb930302cea64de2ddaa05392bdd5b618fb9
mass14 <msumienski@gmail.com>
2015-07-29 21:05:05 -0300

    Revert "GeracaoTec Java"
    This reverts commit 27c9a12bd6aace981f2f77e00bdca326a79d668e.

--- CR/src/br/com/calculareceita/security/Cryptography.java
+++
@@ -1,9 +0,0 @@
-package br.com.calculareceita.security;
-
-import java.security.NoSuchAlgorithmException;
-
-public interface Cryptography {
-
-  String encrypt(String value) throws NoSuchAlgorithmException;
-
-}
